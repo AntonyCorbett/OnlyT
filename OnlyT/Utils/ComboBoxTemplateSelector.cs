51f2768d80202f756f03f88b04b242ee7599f744
Antony Corbett <antony@corbetts.org.uk>
2018-11-28 20:02:14 +0000

    Display of times in drop down talk list

---
+++ OnlyT/Utils/ComboBoxTemplateSelector.cs
@@ -0,0 +1,37 @@
+﻿// adapted from StackOverflow post (courtesy Mark A. Donohoe)
+namespace OnlyT.Utils
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public class ComboBoxTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SelectedItemTemplate { get; set; }
+
+        public DataTemplateSelector SelectedItemTemplateSelector { get; set; }
+
+        public DataTemplate DropdownItemsTemplate { get; set; }
+
+        public DataTemplateSelector DropdownItemsTemplateSelector { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var parent = container;
+
+            // Search up the visual tree, stopping at either a ComboBox or
+            // a ComboBoxItem (or null). This will determine which template to use
+            while (parent != null && !(parent is ComboBoxItem) && !(parent is ComboBox))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            // If you stopped at a ComboBoxItem, you're in the dropdown
+            var inDropDown = parent is ComboBoxItem;
+
+            return inDropDown
+                ? DropdownItemsTemplate ?? DropdownItemsTemplateSelector?.SelectTemplate(item, container)
+                : SelectedItemTemplate ?? SelectedItemTemplateSelector?.SelectTemplate(item, container);
+        }
+    }
+}
---
+++ OnlyT/Utils/ComboBoxTemplateSelectorExtension.cs
@@ -0,0 +1,30 @@
+﻿// adapted from StackOverflow post (courtesy Mark A. Donohoe)
+namespace OnlyT.Utils
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Markup;
+
+    public class ComboBoxTemplateSelectorExtension : MarkupExtension
+    {
+        public DataTemplate SelectedItemTemplate { get; set; }
+
+        public DataTemplateSelector SelectedItemTemplateSelector { get; set; }
+
+        public DataTemplate DropdownItemsTemplate { get; set; }
+
+        public DataTemplateSelector DropdownItemsTemplateSelector { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new ComboBoxTemplateSelector()
+            {
+                SelectedItemTemplate = SelectedItemTemplate,
+                SelectedItemTemplateSelector = SelectedItemTemplateSelector,
+                DropdownItemsTemplate = DropdownItemsTemplate,
+                DropdownItemsTemplateSelector = DropdownItemsTemplateSelector
+            };
+        }
+    }
+}
