804a278cee06e3a5dad312947364a3d84c7eea86
AntonyCorbett <antony@corbetts.org.uk>
2019-01-21 18:00:10 +0000

    guard against transient network errors

--- OnlyT/Utils/WebUtils.cs
+++ OnlyT/Utils/WebUtils.cs
@@ -1,31 +1,42 @@
 ﻿namespace OnlyT.Utils
 {
-    using System.IO;
     using System.Net;
     using System.Text;
-    using System.Xml.Linq;
+    using System.Threading;
 
     internal static class WebUtils
     {
-        /// <summary>
-        /// Downloads an xml file, specifying a user agent string
-        /// </summary>
-        /// <param name="url">Address of file</param>
-        /// <returns>File as XDocument</returns>
-        public static XDocument XDocLoadWithUserAgent(string url)
-        {
-            using (WebClient wc = new WebClient())
-            {
-                wc.Encoding = Encoding.UTF8;
-                wc.Headers.Add("user-agent", GetUserAgentString());
-                string data = wc.DownloadString(url);
-                return XDocument.Load(new MemoryStream(Encoding.UTF8.GetBytes(data)));
-            }
-        }
-
         public static string LoadWithUserAgent(string url)
         {
-            using (WebClient wc = new WebClient())
+            const int attempts = 3;
+
+            string result = null;
+
+            // multiple attempts to guard against transient exceptions.
+            for (var n = 0; n < attempts && result == null; ++n)
+            {
+                try
+                {
+                    result = InternalLoadWithUserAgent(url);
+                }
+                catch (WebException)
+                {
+                    if (n == attempts - 1)
+                    {
+                        throw;
+                    }
+
+                    // ignore
+                    Thread.Sleep(100);
+                }
+            }
+
+            return result;
+        }
+
+        private static string InternalLoadWithUserAgent(string url)
+        {
+            using (var wc = new WebClient())
             {
                 wc.Encoding = Encoding.UTF8;
                 wc.Headers.Add("user-agent", GetUserAgentString());
@@ -35,7 +46,7 @@
 
         private static string GetUserAgentString()
         {
-            return "OnlyT (+http://cv8.org.uk/soundbox)";
+            return "OnlyT (+https://soundboxsoftware.com)";
         }
     }
 }
