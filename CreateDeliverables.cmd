179894fbdf0c3c487a2242385f80fe4c132f886e
Antony Corbett <antony@corbetts.org.uk>
2022-03-30 09:01:08 +0100

    pipeline

--- CreateDeliverables.cmd
+++ CreateDeliverables.cmd
@@ -1,4 +1,9 @@
 REM Run from dev command line
+
+@ECHO OFF
+
+VERIFY ON
+
 D:
 cd \ProjectsPersonal\OnlyT
 rd OnlyT\bin /q /s
@@ -6,20 +11,49 @@ rd Installer\Output /q /s
 
 REM build
 
+ECHO.
+ECHO Publishing OnlyT
 dotnet publish OnlyT\OnlyT.csproj -p:PublishProfile=FolderProfile -c:Release
+IF %ERRORLEVEL% NEQ 0 goto ERROR
+
+ECHO.
+ECHO Publishing OnlyTFirewallPorts
 dotnet publish OnlyTFirewallPorts\OnlyTFirewallPorts.csproj -p:PublishProfile=FolderProfile -c:Release
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM copy items into delivery
+ECHO.
+ECHO Copying items into delivery
 xcopy OnlyTFirewallPorts\bin\Release\net5.0\publish\*.* OnlyT\bin\Release\net5.0-windows\publish /q /s /y /d
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM delete unwanted NAudio x64
-del OnlyT\OnlyT\bin\Release\net5.0-windows\publish\libmp3lame.64.dll
+ECHO.
+ECHO Removing unwanted x64 DLLs
+del OnlyT\bin\Release\net5.0-windows\publish\libmp3lame.64.dll
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM delete unwanted translations
-rd OnlyT\OnlyT\bin\Release\net5.0-windows\publish\id-ID /q /s
+ECHO.
+ECHO Removing unwanted translations
+rd OnlyT\bin\Release\net5.0-windows\publish\id-ID /q /s
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM Create installer
-"C:\Program Files (x86)\Inno Setup 6\iscc" Installer\onlytsetup.iss
+ECHO.
+ECHO Create installer
+"D:\Program Files (x86)\Inno Setup 6\iscc" Installer\onlytsetup.iss
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM create portable zip
+ECHO.
+ECHO Create portable zip
 powershell Compress-Archive -Path OnlyT\bin\Release\net5.0-windows\publish\* -DestinationPath Installer\Output\OnlyTPortable.zip 
+IF %ERRORLEVEL% NEQ 0 goto ERROR
+
+goto SUCCESS
+
+:ERROR
+ECHO.
+ECHO ******************
+ECHO An ERROR occurred!
+ECHO ******************
+
+:SUCCESS
+
+PAUSE
