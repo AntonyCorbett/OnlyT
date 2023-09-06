ad112a0fec450064aa595daf076d6edf6079a568
Antony Corbett <antony@corbetts.org.uk>
2023-09-06 16:46:52 +0100

    refs #508

--- CreateDeliverables.cmd
+++ CreateDeliverables.cmd
@@ -49,6 +49,38 @@ rd Installer\Staging\no-NO /q /s
 rd Installer\Staging\pap-PAP /q /s
 
 ECHO.
+ECHO Copying Satellite assemblies for language files
+ECHO Czech
+xcopy Installer\Staging\cs\*.*  Installer\Staging\cs-CZ /q
+rd Installer\Staging\cs /q /s
+ECHO German
+xcopy Installer\Staging\de\*.*  Installer\Staging\de-DE /q
+rd Installer\Staging\de /q /s
+ECHO French
+xcopy Installer\Staging\fr\*.*  Installer\Staging\fr-FR /q
+rd Installer\Staging\fr /q /s
+ECHO Italian
+xcopy Installer\Staging\it\*.*  Installer\Staging\it-IT /q
+rd Installer\Staging\it /q /s
+ECHO Polish
+xcopy Installer\Staging\pl\*.*  Installer\Staging\pl-PL /q
+rd Installer\Staging\pl /q /s
+ECHO Russian
+xcopy Installer\Staging\ru\*.*  Installer\Staging\ru-RU /q
+rd Installer\Staging\ru /q /s
+ECHO Spanish
+xcopy Installer\Staging\es\*.*  Installer\Staging\es-ES /q
+xcopy Installer\Staging\es\*.*  Installer\Staging\es-MX /q
+rd Installer\Staging\es /q /s
+ECHO Turkish
+xcopy Installer\Staging\tr\*.*  Installer\Staging\tr-TR /q
+rd Installer\Staging\tr /q /s
+ECHO Korean
+xcopy Installer\Staging\ko\*.*  Installer\Staging\ko-KR /q
+rd Installer\Staging\ko /q /s
+
+
+ECHO.
 ECHO Creating installer
 "D:\Program Files (x86)\Inno Setup 6\iscc" Installer\onlytsetup.iss
 IF %ERRORLEVEL% NEQ 0 goto ERROR
