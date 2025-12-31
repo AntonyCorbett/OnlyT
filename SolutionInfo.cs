f5f57822abf82c060efae21e3247c427f9590d81
Antony Corbett <antony@corbetts.org.uk>
2025-12-31 17:08:24 +0000

    bump ver

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,10 +3,10 @@ using System.Resources;
 
 [assembly: AssemblyCompany("SoundBox")]
 [assembly: AssemblyProduct("OnlyT")]
-[assembly: AssemblyCopyright("Copyright © 2019, 2025 Antony Corbett")]
+[assembly: AssemblyCopyright("Copyright © 2019, 2026 Antony Corbett")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: NeutralResourcesLanguage("en-GB")]
 
-[assembly: AssemblyVersion("2.4.0.14")]
+[assembly: AssemblyVersion("2.4.0.15")]
